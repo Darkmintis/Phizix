6171da96e371899e9f3865d10cd908f2f83fba5f
Darkmintis <darkmintis@gmail.com>
2026-04-01 17:18:33 +0545

    add article model

---
+++ lib/views/articles/models/article_model.dart
@@ -0,0 +1,41 @@
+class Article {
+  final String title;
+  final String slug;
+  final String excerpt;
+  final String content;
+  final String featureImage;
+  final DateTime publishedAt;
+
+  Article({
+    required this.title,
+    required this.slug,
+    required this.excerpt,
+    required this.content,
+    required this.featureImage,
+    required this.publishedAt,
+  });
+
+  factory Article.fromJson(Map<String, dynamic> json){
+    return Article( 
+      title: json['title'] ?? '',
+      slug: json['slug'] ?? '',
+      excerpt: json['excerpt'] ?? '',
+      content: json['content'] ?? '', 
+      featureImage: json['featureImage'] ?? '',
+      publishedAt: json['publishedAt'] != null
+      ? DateTime.parse(json['publishedAt'] as String)
+      : DateTime.now(),
+    );
+  }
+
+  Map<String, dynamic> toJson(){
+    return{
+      'title': title,
+      'slug': slug,
+      'excerpt': excerpt,
+      'content': content,
+      'featureImage': featureImage,
+      'publishedAt': publishedAt.toIso8601String(),
+    };
+  }
+}
