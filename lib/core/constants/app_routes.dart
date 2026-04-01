922fb248252b26dae9d6fe103568140d5d5071a5
Darkmintis <darkmintis@gmail.com>
2026-04-01 17:13:49 +0545

    add app routes

---
+++ lib/core/constants/app_routes.dart
@@ -0,0 +1,6 @@
+class AppRoutes {
+  static const String articles = '/articles';
+  static const String categories = '/categories';
+  static const String tags = '/tags';
+  static const String authors = '/authors';
+}
