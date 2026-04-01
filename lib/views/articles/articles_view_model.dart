52d5fad8924989be3c0364b1f302c3acad59b58a
Darkmintis <darkmintis@gmail.com>
2026-04-01 17:18:24 +0545

    add article view model

---
+++ lib/views/articles/articles_view_model.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'repositories/article_repository.dart';
+import 'models/article_model.dart';
+
+enum ViewState { idle, loading, success, error }
+
+class ArticlesViewModel extends ChangeNotifier {
+  final ArticleRepository _repository;
+  
+  ViewState _state = ViewState.idle;
+  List<Article> _articles = [];
+  String _errorMessage = '';
+
+  ArticlesViewModel(this._repository);
+
+  // Getters
+  ViewState get state => _state;
+  List<Article> get articles => _articles;
+  String get errorMessage => _errorMessage;
+  bool get isLoading => _state == ViewState.loading;
+
+  Future<void> loadArticles() async {
+    _state = ViewState.loading;
+    notifyListeners();
+    
+    try {
+      _articles = await _repository.getArticles(page: 1);
+      _state = ViewState.success;
+      _errorMessage = '';
+    } catch (e) {
+      _state = ViewState.error;
+      _errorMessage = e.toString();
+    }
+      notifyListeners();
+    }
+
+  Future<void> refreshArticles() => loadArticles();
+  
+  Future<void> retry() => loadArticles();
+}
