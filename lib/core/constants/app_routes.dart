f65c81ae37878f42535794cc49a778e3b52aafbc
Darkmintis <darkmintis@gmail.com>
2026-04-09 15:26:45 +0545

    refactor: add missing route constants for article details and filtering

--- lib/core/constants/app_routes.dart
+++ lib/core/constants/app_routes.dart
@@ -1,6 +1,14 @@
 class AppRoutes {
+  static const String home = '/';
   static const String articles = '/articles';
   static const String categories = '/categories';
   static const String tags = '/tags';
   static const String authors = '/authors';
+
+  static const String articleDetails = '/article/details';
+  static const String filteredArticles = '/article/filtered';
+
+  static const String argSlug = 'slug';
+  static const String argTitle = 'title';
+  static const String argFilterType = 'filterType';
 }
