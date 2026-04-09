153144c144d36187093394588095ccf46b79ca4b
Darkmintis <darkmintis@gmail.com>
2026-04-09 15:27:52 +0545

    refactor: enhance debug logging in Dio interceptor for request, response, and error handling

--- lib/core/network/dio_interceptor.dart
+++ lib/core/network/dio_interceptor.dart
@@ -5,17 +5,23 @@ import '../services/api_exception.dart';
 class AppInterceptor extends Interceptor{
   @override
     void onRequest(RequestOptions options, RequestInterceptorHandler handler){
+        if (kDebugMode) {
         debugPrint('Request: ${options.method} ${options.path}');
+        }
         handler.next(options);
       }
       @override
     void onResponse(Response response, ResponseInterceptorHandler handler) {
+        if (kDebugMode) {
         debugPrint('Response: ${response.statusCode} ${response.requestOptions.path}');
+        }
         handler.next(response);
       }
       @override
      void onError(DioException err, ErrorInterceptorHandler handler){
+        if (kDebugMode) {
         debugPrint('Error: ${err.message}');
+        }
 
         final exception = _mapError(err);
         handler.next(err.copyWith(error: exception));
